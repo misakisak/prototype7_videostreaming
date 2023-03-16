b66d5d135399ca7ce8202ee8b8269b8b7c90fc21
misakisak <dr.misakisak@gmail.com>
2023-03-15 20:29:41 -0400

    xcode経由でfile.swiftを作らなきゃいけなかった様でそれを足した。

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 //implemented from the following website
 //https://docs.agora.io/en/interactive-live-streaming/get-started/get-started-sdk?platform=react-native
 
-
 import React, {useRef, useState, useEffect} from 'react';
 import {
     SafeAreaView,
@@ -20,145 +19,158 @@ import {
     ChannelProfileType,
 } from 'react-native-agora';
 
-const appId = 'bcd7144c85a0467084ccf5c84a8a4f75';
+const appId = 'a9ca2431c4b948e7bd9d3036a841130f';
 const channelName = 'abc';
-const token = '007eJxTYNj7v3iJ1d8bYv33WKOVjn9awMy5tz73nXShaW7CKv8Pq0QUGJKSU8wNTUySLUwTDUzMzA0sTJKT00yTLUwSLRJN0sxNzVJZUxoCGRn+qhoyMTJAIIjPzJCYlMzAAABHzh5K';
+const token = '007eJxTYHC+fDfGvJpF0WT/g1sPzbTtpK4ll/fIT9wRcZlVp3nGyjIFhkTL5EQjE2PDZJMkSxOLVPOkFMsUYwNjs0QLE0NDY4O0flehlIZARoYpBX3MjAwQCOIzMyQmJTMwAADGUx0h';
 const uid = 0;
 
+const engine = createAgoraRtcEngine();
+engine.initialize({appId: 'a9ca2431c4b948e7bd9d3036a841130f'});
+
 const App = () => {
-    // const agoraEngineRef = useRef(IRtcEngine()); // Agora engine instance
-    const engine = createAgoraRtcEngine();
-    engine.initialize({appId: 'bcd7144c85a0467084ccf5c84a8a4f75'});
+    // const agoraEngineRef = useRef<IRtcEngine>(); // Agora engine instance
     const [isJoined, setIsJoined] = useState(false); // Indicates if the local user has joined the channel
     const [isHost, setIsHost] = useState(true); // Client role
     const [remoteUid, setRemoteUid] = useState(0); // Uid of the remote user
     const [message, setMessage] = useState(''); // Message to the user
 
-    useEffect(() => {
-      // Initialize Agora engine when the app starts
-      setupVideoSDKEngine();
-    });
-   
-    const setupVideoSDKEngine = async () => {
-      try {
-      // use the helper function to get permissions
-      if (Platform.OS === 'android') { await getPermission()};
-      agoraEngineRef.current = createAgoraRtcEngine();
-      const agoraEngine = agoraEngineRef.current;
-      agoraEngine.registerEventHandler({
-          onJoinChannelSuccess: () => {
-              showMessage('Successfully joined the channel ' + channelName);
-              setIsJoined(true);
-          },
-          onUserJoined: (_connection, Uid) => {
-              showMessage('Remote user joined with uid ' + Uid);
-              setRemoteUid(Uid);
-          },
-          onUserOffline: (_connection, Uid) => {
-              showMessage('Remote user left the channel. uid: ' + Uid);
-              setRemoteUid(0);
-          },
-      });
-      agoraEngine.initialize({
-          appId: appId,
-          channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
-      });
-      agoraEngine.enableVideo();
-      } catch (e) {
-          console.log(e);
-      }
-    };
-
     const join = async () => {
-      if (isJoined) {
-          return;
-      }
-      try {
-          agoraEngineRef.current?.setChannelProfile(
-              ChannelProfileType.ChannelProfileLiveBroadcasting,
-          );
-          if (isHost) {
-              agoraEngineRef.current?.startPreview();
-              agoraEngineRef.current?.joinChannel(token, channelName, uid, {
-                  clientRoleType: ClientRoleType.ClientRoleBroadcaster});
-          } else {
-              agoraEngineRef.current?.joinChannel(token, channelName, uid, {
-                  clientRoleType: ClientRoleType.ClientRoleAudience});
-          }
-      } catch (e) {
-          console.log(e);
-      }
+        if (isJoined) {
+            return;
+        }
+        try {
+            // agoraEngineRef.current?.setChannelProfile(
+            engine.current?.setChannelProfile(
+                ChannelProfileType.ChannelProfileLiveBroadcasting,
+            );
+            if (isHost) {
+                // agoraEngineRef.current?.startPreview();
+                // agoraEngineRef.current?.joinChannel(token, channelName, uid, {
+                //     clientRoleType: ClientRoleType.ClientRoleBroadcaster});
+                engine.current?.startPreview();
+                engine.current?.joinChannel(token, channelName, uid, {
+                    clientRoleType: ClientRoleType.ClientRoleBroadcaster});
+            } else {
+                // agoraEngineRef.current?.joinChannel(token, channelName, uid, {
+                //     clientRoleType: ClientRoleType.ClientRoleAudience});
+                engine.current?.joinChannel(token, channelName, uid, {
+                    clientRoleType: ClientRoleType.ClientRoleAudience});
+            }
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     const leave = () => {
-      try {
-          agoraEngineRef.current?.leaveChannel();
-          setRemoteUid(0);
-          setIsJoined(false);
-          showMessage('You left the channel');
-      } catch (e) {
-          console.log(e);
-      }
+        try {
+            // agoraEngineRef.current?.leaveChannel();
+            engine.current?.leaveChannel();
+            setRemoteUid(0);
+            setIsJoined(false);
+            showMessage('You left the channel');
+            // setMessage('You left the channel');
+        } catch (e) {
+            console.log(e);
+        }
     };
 
+    
+    useEffect(() => {
+        // Initialize Agora engine when the app starts
+        setupVideoSDKEngine();
+     });
+     
+     const setupVideoSDKEngine = async () => {
+        try {
+        // use the helper function to get permissions
+        if (Platform.OS === 'android') { await getPermission()};
+        // agoraEngineRef.current = createAgoraRtcEngine();
+        // const agoraEngine = agoraEngineRef.current;
+        engine.current = createAgoraRtcEngine();
+        const agoraEngine = engine.current;
+        agoraEngine.registerEventHandler({
+            onJoinChannelSuccess: () => {
+                showMessage('Successfully joined the channel ' + channelName);
+                // setMessage('Successfully joined the channel ' + channelName)
+                setIsJoined(true);
+            },
+            onUserJoined: (_connection, Uid) => {
+                showMessage('Remote user joined with uid ' + Uid);
+                // setMessage('Remote user joined with uid ' + Uid);
+                setRemoteUid(Uid);
+            },
+            onUserOffline: (_connection, Uid) => {
+                showMessage('Remote user left the channel. uid: ' + Uid);
+                // setMessage('Remote user left the channel. uid: ' + Uid);
+                setRemoteUid(0);
+            },
+        });
+        agoraEngine.initialize({
+            appId: appId,
+            channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+        });
+        agoraEngine.enableVideo();
+        } catch (e) {
+            console.log(e);
+        }
+     };
+     
 
-  
-  
     return (
-      <SafeAreaView style={styles.main}>
-          <Text style={styles.head}>
-              Agora Interactive Live Streaming Quickstart
-          </Text>
-          <View style={styles.btnContainer}>
-              <Text onPress={join} style={styles.button}>
-                  Join
-              </Text>
-              <Text onPress={leave} style={styles.button}>
-                  Leave
-              </Text>
-          </View>
-          <View style={styles.btnContainer}>
-              <Text>Audience</Text>
-              <Switch
-                  onValueChange={switchValue => {
-                      setIsHost(switchValue);
-                      if (isJoined) {
-                      leave();
-                      }
-                  }}
-                  value={isHost}
-              />
-              <Text>Host</Text>
-          </View>
-        <ScrollView
-              style={styles.scroll}
-              contentContainerStyle={styles.scrollContainer}>
-              {isJoined && isHost ? (
-              <React.Fragment key={0}>
-                  <RtcSurfaceView canvas={{uid: 0}} style={styles.videoView} />
-                  <Text>Local user uid: {uid}</Text>
-              </React.Fragment>
-              ) : (
-                  <Text>{isHost ? 'Join a channel' : ''}</Text>
-              )}
-              {isJoined && !isHost && remoteUid !== 0 ? (
-                  <React.Fragment key={remoteUid}>
-                      <RtcSurfaceView
-                          canvas={{uid: remoteUid}} style={styles.videoView} />
-                      <Text>Remote user uid: {remoteUid}</Text>
-                  </React.Fragment>
-              ) : (
-                  <Text>{isJoined && !isHost ? 'Waiting for a remote user to join' : ''}</Text>
-              )}
-              <Text style={styles.info}>{message}</Text>
-          </ScrollView>
-      </SafeAreaView>
+        <SafeAreaView style={styles.main}>
+            <Text style={styles.head}>
+                Agora Interactive Live Streaming Quickstart
+            </Text>
+            <View style={styles.btnContainer}>
+                <Text onPress={join} style={styles.button}>
+                    Join
+                </Text>
+                <Text onPress={leave} style={styles.button}>
+                    Leave
+                </Text>
+            </View>
+            <View style={styles.btnContainer}>
+                <Text>Audience</Text>
+                <Switch
+                    onValueChange={switchValue => {
+                        setIsHost(switchValue);
+                        if (isJoined) {
+                        leave();
+                        }
+                    }}
+                    value={isHost}
+                />
+                <Text>Host</Text>
+            </View>
+            <ScrollView
+                style={styles.scroll}
+                contentContainerStyle={styles.scrollContainer}>
+                {isJoined && isHost ? (
+                <React.Fragment key={0}>
+                    <RtcSurfaceView canvas={{uid: 0}} style={styles.videoView} />
+                    <Text>Local user uid: {uid}</Text>
+                </React.Fragment>
+                ) : (
+                    <Text>{isHost ? 'Join a channel' : ''}</Text>
+                )}
+                {isJoined && !isHost && remoteUid !== 0 ? (
+                    <React.Fragment key={remoteUid}>
+                        <RtcSurfaceView
+                            canvas={{uid: remoteUid}} style={styles.videoView} />
+                        <Text>Remote user uid: {remoteUid}</Text>
+                    </React.Fragment>
+                ) : (
+                    <Text>{isJoined && !isHost ? 'Waiting for a remote user to join' : ''}</Text>
+                )}
+                <Text style={styles.info}>{message}</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
-  
+    
 
-    // function showMessage(msg: string) {
-    //     setMessage(msg);
-    // }
+    function showMessage(msg) {
+        setMessage(msg);
+    }
 };
 
 const styles = StyleSheet.create({
@@ -180,12 +192,12 @@ const styles = StyleSheet.create({
 });
 
 const getPermission = async () => {
-  if (Platform.OS === 'android') {
-      await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-      ]);
-  }
+    if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+        ]);
+    }
 };
 
 
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  prototype7videostreaming
+//
+//  Created by Misaki Tatsumi on 3/15/23.
+//
+
+import Foundation
